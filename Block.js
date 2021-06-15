033de03de28abf5bb94186d1152070066c1214dd
snigdhaArya <76739977+snigdhaArya@users.noreply.github.com>
2021-06-15 14:43:33 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -16,7 +16,7 @@ class Block{
 display(){
   var pos= this.body.position;
   rectMode(CENTER);
-  rect(pos.x,pos.y,this.width, this.height);
+//   rect(pos.x,pos.y,this.width, this.height);
  
   if(this.body.speed<3){
     image(this.image,this.body.position.x,this.body.position.y,this.width,this.height) 
